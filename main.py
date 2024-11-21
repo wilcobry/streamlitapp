f13ceae1c724e40cf3a8ca7c5d346bd2fed715df
wilcobry <wilcobry@gmail.com>
2024-11-20 18:42:14 -0700

    main changes

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import numpy as np
-import streamlit as st
 import pandas as pd
 import zipfile
 import plotly.express as px
@@ -9,6 +7,7 @@ from io import BytesIO
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from my_plots import *
+import streamlit as st
 
 @st.cache_data
 def load_name_data():
@@ -37,8 +36,25 @@ def ohw(df):
 data = load_name_data()
 ohw_data = ohw(data)
 
+st.title('My Cool Name App')
+
+with st.sidebar:
+    input_name = st.text_input('Enter a name:', 'Mary')
+    year_input = st.slider("Year", min_value=1880, max_value=2023, value=2000)
+    n_names = st.radio('Number of names per sex', [3, 4, 5, 6])
 
 
+tab1, tab2 = st.tabs(['Names', 'Year'])
 
+with tab1: 
+    name_data = data[data['name']==input_name].copy()
+    fig = px.line(name_data, x='year', y='count', color='sex')
+    st.plotly_chart(fig)
 
+with tab2:
+    fig2 = top_names_plot(data, year=year_input, n=n_names)
+    st.plotly_chart(fig2)
 
+    st.write('Unique Names Table')
+    output_table = unique_names_summary(data, 2000)
+    st.dataframe(output_table)
