d7c485529f078ae143268006abb1dc1ecf5a345b
wilcobry <wilcobry@gmail.com>
2024-11-20 18:06:18 -0700

    committed

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import zipfile
+import plotly.express as px
+import matplotlib.pyplot as plt
+import requests
+from io import BytesIO
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+from my_plots import *
+
+@st.cache_data
+def load_name_data():
+    names_file = 'https://www.ssa.gov/oact/babynames/names.zip'
+    response = requests.get(names_file)
+    with zipfile.ZipFile(BytesIO(response.content)) as z:
+        dfs = []
+        files = [file for file in z.namelist() if file.endswith('.txt')]
+        for file in files:
+            with z.open(file) as f:
+                df = pd.read_csv(f, header=None)
+                df.columns = ['name','sex','count']
+                df['year'] = int(file[3:7])
+                dfs.append(df)
+        data = pd.concat(dfs, ignore_index=True)
+    data['pct'] = data['count'] / data.groupby(['year', 'sex'])['count'].transform('sum')
+    return data
+
+@st.cache_data
+def ohw(df):
+    nunique_year = df.groupby(['name', 'sex'])['year'].nunique()
+    one_hit_wonders = nunique_year[nunique_year == 1].index
+    one_hit_wonder_data = df.set_index(['name', 'sex']).loc[one_hit_wonders].reset_index()
+    return one_hit_wonder_data
+
+data = load_name_data()
+ohw_data = ohw(data)
+
+
+
+
+
+
