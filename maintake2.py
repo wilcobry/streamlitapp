a8ff945b3f91736fca221fdcd292e9643885ff94
wilcobry <wilcobry@gmail.com>
2024-12-04 18:09:50 -0700

    updated

--- maintake2.py
+++ maintake2.py
@@ -20,13 +20,13 @@ with st.sidebar:
     input_city = st.text_input('Enter a city in Utah County:', 'Provo')
     n_beds = st.radio('Number of beds', [1, 2, 3, 4, 5, 6])
     price_range = st.slider("Select a price range:",
-                            min_value = data['Price'].min(),
-                            max_value = data['Price'].max(),
+                            min_value = int(data['Price'].min()),
+                            max_value = int(data['Price'].max()),
                             value = (400000, 600000),
                             step = 10000)
     square_feet = st.slider("Select a house size:",
-                            min_value = data['Sq_Footage'].min(),
-                            max_value = data['Sq_Footage'].max(),
+                            min_value = int(data['Sq_Footage'].min()),
+                            max_value = int(data['Sq_Footage'].max()),
                             value = (1000, 2000),
                             step = 500)
 
