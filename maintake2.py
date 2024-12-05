68235fbd00eee0869ef60e71cd7b867bb16598cd
wilcobry <wilcobry@gmail.com>
2024-12-04 18:17:45 -0700

    commited

--- maintake2.py
+++ maintake2.py
@@ -47,10 +47,12 @@ with tab2:
 
     st.header('Houses Within Price Range')
     st.dataframe(price_data)
+    st.write(price_data.head())
 
 with tab3:
     st.header("Price by Square Footage For Selected Bedroom Count")
     sq_data = data[data['Sq_Footage']].between(square_feet[0], square_feet[1])
     fig3 = px.scatter(sq_data, x='Sq_Footage', y='Price', hover_data = 'City')
     st.plotly_chart(fig3)
+    st.write(sq_data.head())
 
