66b29aa477d0a3a087964102ab2953aa9721777d
wilcobry <wilcobry@gmail.com>
2024-12-04 18:22:05 -0700

    done

--- maintake2.py
+++ maintake2.py
@@ -41,7 +41,7 @@ with tab1:
 
 with tab2:
     st.header("Distribution of Bedrooms Within Price Range")
-    price_data = data[data['Price']].between(price_range[0], price_range[1])
+    price_data = data[data['Price']].between(price_range[0], price_range[1]).copy()
     fig2 = px.histogram(price_data, x= 'Beds')
     st.plotly_chart(fig2)
 
@@ -51,7 +51,7 @@ with tab2:
 
 with tab3:
     st.header("Price by Square Footage For Selected Bedroom Count")
-    sq_data = data[data['Sq_Footage']].between(square_feet[0], square_feet[1])
+    sq_data = data[data['Sq_Footage']].between(square_feet[0], square_feet[1]).copy()
     fig3 = px.scatter(sq_data, x='Sq_Footage', y='Price', hover_data = 'City')
     st.plotly_chart(fig3)
     st.write(sq_data.head())
